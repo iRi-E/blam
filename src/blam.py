b54390a6da4a0a31bb9edcf3a9772c09cf182009
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2018-12-23 18:11:12 +0900

    Bump version to 0.7

--- src/blam.py
+++ src/blam.py
@@ -25,7 +25,7 @@ import cmath
 bl_info = {
     'name': 'BLAM for Blender 2.8 - The Blender camera calibration toolkit',
     'author': 'Per Gantelius, IRIE Shinsuke',
-    'version': (0, 6, 2),
+    'version': (0, 7, 0),
     'blender': (2, 80, 0),
     'location': 'Move Clip Editor > Tools > Static Camera Calibration and 3D View > Tool Shelf > Photo Modeling Tools',
     'description': 'Reconstruct 3D geometry and estimate camera orientation and focal length based on photographs',
