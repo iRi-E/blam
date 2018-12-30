108dc315acc2ed8432744438470cb9edc474db03
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2018-12-30 23:26:17 +0900

    Bump version to 0.8

--- src/blam.py
+++ src/blam.py
@@ -25,7 +25,7 @@ import numpy as np
 bl_info = {
     'name': 'BLAM for Blender 2.8 - The Blender camera calibration toolkit',
     'author': 'Per Gantelius, IRIE Shinsuke',
-    'version': (0, 7, 1),
+    'version': (0, 8, 0),
     'blender': (2, 80, 0),
     'location': 'Move Clip Editor > Tools > Static Camera Calibration and 3D View > Tool Shelf > Photo Modeling Tools',
     'description': 'Reconstruct 3D geometry and estimate camera orientation and focal length based on photographs',
